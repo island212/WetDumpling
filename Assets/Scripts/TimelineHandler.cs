d565aa771798aed3b00b880a2d15637a52ce1fff
Felix Brault <FeelixBrault@gmail.com>
2019-01-26 00:03:11 -0500

    Added TimelineHandler

---
+++ Assets/Scripts/TimelineHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineHandler : MonoBehaviour
+{
+    [SerializeField]
+    private int maxCards;
+
+    void Start()
+    {
+        removeTopCard();    
+    }
+
+    public int getNumberOfCards()
+    {
+        return transform.childCount;
+    }    
+
+    public bool addCard(GameObject card)
+    {
+        if (getNumberOfCards() > maxCards)
+        {
+            return false;
+        }
+
+        GameObject newCard = Instantiate(card, transform);
+        Transform[] childrens = GetComponentsInChildren<Transform>();
+        Transform closestCard = null;
+        float smallestDistance = Mathf.Infinity;
+        float currentDistance;
+
+        foreach (Transform i in childrens)
+        {
+            currentDistance = (i.position - card.transform.position).magnitude;
+            if (currentDistance < smallestDistance)
+            {
+                closestCard = i;
+                smallestDistance = currentDistance;
+            }
+        }
+
+        if (Mathf.Sign(closestCard.transform.position.x - card.transform.position.x) == 1)
+        {
+            newCard.transform.SetSiblingIndex(closestCard.transform.GetSiblingIndex() + 1);
+        }
+        else
+        {
+            newCard.transform.SetSiblingIndex(closestCard.transform.GetSiblingIndex() - 1);
+        }
+        return true;
+    }
+
+    public bool removeTopCard()
+    {
+        if (getNumberOfCards() <= 0)
+        {
+            return false;
+        }
+
+        Transform[] childrens = GetComponentsInChildren<Transform>();
+        Destroy(childrens[1].gameObject);
+
+        return true;
+    }
+}
