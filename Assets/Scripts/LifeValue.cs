725b71fb967254cbf0adbd4964547bbcc846a3a4
Felix Brault <FeelixBrault@gmail.com>
2019-01-26 20:08:47 -0500

    Added life and button fading

---
+++ Assets/Scripts/LifeValue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeValue : MonoBehaviour
+{
+    private int currentLife;
+    private Text lifeText;
+
+    void Start()
+    {
+        currentLife = transform.parent.GetComponent<CharacterComponent>().characterData.maxHealth;
+        lifeText = transform.GetChild(0).GetChild(0).GetComponent<Text>();
+        lifeText.text = currentLife.ToString(); ;
+    }
+
+    public void setLife(int life)
+    {
+        currentLife += life;
+        lifeText.text = currentLife.ToString();
+    }
+}
